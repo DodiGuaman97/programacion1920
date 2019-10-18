43414bc1f4bd0f55f8a64e9a1a37c417146be55d
DodiGuaman97 <dodiguaman97@gmail.com>
2019-10-17 20:37:00 -0500

    nuevos cambios en clase IntroJava.java

---
+++ Intro Java/src/intro/java/IntroJava.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package intro.java;
+
+/**
+ *
+ * @author Salas
+ */
+public class IntroJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Bienvenido a la programacion Java");
+        System.out.println("Este codigo va ha ser subido en githuben el repositorio programacion1920");
+        
+        
+        
+    }
+    
+}

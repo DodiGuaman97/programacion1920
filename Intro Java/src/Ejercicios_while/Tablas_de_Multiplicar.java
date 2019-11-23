a2b0efd334cc17db29519a01b9399276476ffaad
DodiGuaman97 <56704390+DodiGuaman97@users.noreply.github.com>
2019-11-22 20:14:11 -0500

    Create Tablas_de_Multiplicar.java

---
+++ Intro Java/src/Ejercicios_while/Tablas_de_Multiplicar.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ejercicios_while;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Tablas_de_Multiplicar {
+    public static void main(String[] args) {
+        int contador=2;
+        int acumulador=0;
+        int par=0;
+        
+        while(contador<=100);{
+        if (contador % 2 ==0);
+        par=contador;
+        acumulador=acumulador+par;
+        
+    }
+         contador ++;
+        
+         System.out.println("la suma de pares del 2 al 100 suma"+par);
+         
+} 
+    
+    
+}
+    
+

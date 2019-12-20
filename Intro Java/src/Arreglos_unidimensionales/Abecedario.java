3f4928b2282aaa1da5c4dab5aa5380649cf56bbb
DodiGuaman97 <56704390+DodiGuaman97@users.noreply.github.com>
2019-12-20 15:07:26 -0500

    Create Abecedario.java

---
+++ Intro Java/src/Arreglos_unidimensionales/Abecedario.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Arreglos_unidimensionales;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Abecedario {
+
+    public static void main(String[] args) {
+        char abecedario[] = new char[26];
+        int indice = 0;
+        for (char letra = 'a'; letra <= 'z'; letra++) {
+            // System.out.println(letra);
+            // operacion de escritura
+            abecedario[indice] = letra;
+            indice++;
+        }
+        //operacion lectura
+        int tam = abecedario.length; //obtenemos tamaño del arreglo
+        for (int ind = 0; ind < tam; ind++) {
+            if (ind == (tam - 1)) {
+                System.out.print(abecedario[ind]+", ");
+
+            } else {
+                System.out.print(abecedario[ind] );
+            }
+
+        }
+    }
+}

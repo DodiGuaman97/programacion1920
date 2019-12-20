3a77215f6467cd67f1651e9ca852294120f63a37
DodiGuaman97 <56704390+DodiGuaman97@users.noreply.github.com>
2019-12-20 15:05:31 -0500

    Create Ejercicio1.java

---
+++ Intro Java/src/Deber_Arreglos/Ejercicio1.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Deber_Arreglos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        String a[] = new String[4];
+        int user = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("Ingresar nombre nr: " + i);
+            a[i] = teclado.nextLine();
+
+        }
+        System.out.println("Ingresar numero de lista");
+        user = teclado.nextInt();
+        System.out.println("el nombre de la lista es: " + a[user]);
+
+    }
+
+}

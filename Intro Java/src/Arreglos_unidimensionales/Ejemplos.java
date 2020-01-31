d158a965f98460a23ec06cb3b0a92724c04b6998
DodiGuaman97 <56704390+DodiGuaman97@users.noreply.github.com>
2020-01-31 14:44:58 -0500

    Update Ejemplos.java

--- Intro Java/src/Arreglos_unidimensionales/Ejemplos.java
+++ Intro Java/src/Arreglos_unidimensionales/Ejemplos.java
@@ -1,11 +1,11 @@
-/*
+ /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package Arreglos_unidimensionales;
 
-/**
+/**  
  *
  * @author Usuario
  */
@@ -18,13 +18,12 @@ public class Ejemplos {
         //2.declaracion de arreglo, indicandon su longuitud
         String estudiantes[]=new String[20];
         estudiantes[0] = " Inicio ";
-        estudiantes[10] = " Dodi Guaman ";
+        estudiantes[7] = " Dodi Guaman ";
         estudiantes[19] = " Final ";
         estudiantes[0] = " Nuevo Valor ";
         
         for (int indice = 0; indice < estudiantes.length; indice++) {
-            System.out.println("estudiantes["+ indice +"]="+estudiantes[indice]);
-            
+            System.out.println("estudiantes["+ indice +"]="+estudiantes[indice]);        
         }
        // System.out.println(universidades[0]);
        //System.out.println(universidades[1]);

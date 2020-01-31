a8a303f676ee78ed1fca3c95a49a55f57af2e629
DodiGuaman97 <56704390+DodiGuaman97@users.noreply.github.com>
2020-01-31 14:44:01 -0500

    Create Ejercicio1.java

---
+++ Intro Java/src/Métodos/Deber/Ejercicio1.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Métodos.Deber;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        Scanner teclado= new Scanner(System.in);
+        int num1 =1;
+        int num2=0;
+        int num3=0;
+        int num4=0;
+        int i,suma =0;
+        int resta =0;
+        int multiplicacion =0;
+        int div =0;
+        int total =0;
+        for (i = 0; i < num1; i++);
+        if (num1 % i == 0) {
+             div = div + i;
+        }
+         i = i + 1;
+         for (i = 0; i < num2; i++);
+          if (num2 % i == 0) {
+             div = div + i;
+    }
+           i = i + 1;
+         for (i = 0; i < num3; i++);
+          if (num3 % i == 0) {
+             div = div + i;
+    
+    }
+           i = i + 1;
+         for (i = 0; i < num4; i++);
+          if (num4 % i == 0) {
+             div = div + i;
+    }
+          
+    }
+}
